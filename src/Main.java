097f83ce6e8271f450b5bbd978050ca908c99a3b
AbramyanAA <abramyanabramyan@yandex.ru>
2022-03-22 12:57:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(600, 600);
+        frame.add(new MainForm().getMainPanel());
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+    }
+}
